082552c17f870c1c1bf71e620da3c6fed8e7306e
Vendantte <133399500+Vendantte@users.noreply.github.com>
2023-06-14 14:45:53 +0700

    Upload

---
+++ data.js
@@ -0,0 +1,181 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Холл",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.05353134547489802,
+        "pitch": 0.0049282654598616915,
+        "fov": 0.947459034658295
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5131607714368478,
+          "pitch": 0.03903147675813656,
+          "rotation": 1.5707963267948966,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.6918909631775119,
+          "pitch": 0.22292894596255763,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.10093160115908262,
+        "pitch": 0.006787360822535504,
+        "fov": 0.947459034658295
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12070890412454105,
+          "pitch": 0.009222735535505677,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.015643699998166838,
+          "pitch": 0.22923110194276397,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -3.1322657175173703,
+          "pitch": 0.007286281362819835,
+          "rotation": 4.71238898038469,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Коворкинг",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.05738798807247569,
+        "pitch": 0.014129669253030386,
+        "fov": 0.947459034658295
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7850010881432432,
+          "pitch": 0.03744439096751684,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "Кафе",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.02698643316833582,
+        "pitch": 0.039427717200331,
+        "fov": 0.947459034658295
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7484897865477844,
+          "pitch": 0.03762243207438942,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
